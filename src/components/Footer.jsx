eda583fc4243869b9f541d22c091cde66e929948
sakshibadyal <sakshibadyal09@gmail.com>
2025-11-04 10:16:44 +0530

    Addnew

---
+++ src/components/Footer.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import { assets } from '../assets/assets'
+
+const Footer = () => {
+  return (
+    <div className='pt-10 px-4 md:px-20 lg:px-32 bg-gray-900 w-full overflow-hidden' id='Footer'>
+      <div className='container mx-auto flex flex-col md:flex-row justify-between items-start'>
+        <div className='w-full md:w-1/3 md-8 md:mb-0'><img src={assets.logo_dark} alt=''></img>
+        <p className='text-gray-400 mt-4'>
+            Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p></div>
+        <div className='w-full md:w-1/5 mb-8 md:mb-0'>
+            <h3 className='text-white text-lg font-bold mb-4'>Company</h3>
+            <ul className='flex flex-col gap-2 text-gray-400'>
+                <a href='#Header' className='hover:text-white'>Home</a>
+                <a href='#About Us' className='hover:text-white'>About Us</a>
+                <a href='#Contact Us' className='hover:text-white'>Contact Us</a>
+                <a href='#Privacy Policy' className='hover:text-white'>Privacy Policy</a>
+            </ul>
+        </div>
+
+        <div className='w-full md:w-1/3'>
+        <h3 className='text-white text-lg font-bold mb-4'>
+            Subscribe to Our News Letter
+        </h3>
+        <p className='text-gray-400 mb-4 max-w-80'>The Latest new, articcles,resources,sent to your inbox weeklyy</p>
+        <div className='flex gap-2'>
+            <input type='email' placeholder='Enter your email' className='p-2 rounded bg-gray-800 text-gray-400 border border-gray-700 focus:outline-none w-full md:w-auto'></input>
+            <button className='py-2 px-4 rounded bg-blue-500'>Subscribe</button>
+        </div>
+        </div>
+      </div>
+      <div className='border-t border-gray-700 py-4 mt-10 text-center text-gray-500'>
+        CopyRight 2024 GreatStack .All Right Reserved.
+      </div>
+    </div>
+  )
+}
+
+export default Footer
